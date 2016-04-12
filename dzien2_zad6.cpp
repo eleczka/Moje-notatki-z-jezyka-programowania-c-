9ad1b0d2c29ef4b4bfa182ef5c04bc07bf1e1409
eleczka <eleczka@users.noreply.github.com>
2016-04-12 18:24:08 +0200

    Added files via upload

---
+++ dzien2_zad6.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+	double a, suma=0;
+	int licznik, ile;
+	printf("Podaj ile liczb sumowac i usrednic:");
+    scanf("%d", &ile);
+for(licznik=1; licznik<=ile;licznik++){
+    printf("podaj %d liczbe: ",licznik);
+    scanf ("%lf",&a);
+	suma=suma+a;
+}
+	printf("suma=%lf\n", suma);
+ printf("srednia=%lf\n", suma/ile);
+ return 0;
+}
