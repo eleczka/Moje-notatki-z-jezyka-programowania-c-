cc12ad864fff93e72a4a7dbc62acafd9f9226c9e
eleczka <eleczka@users.noreply.github.com>
2016-04-12 18:22:01 +0200

    Added files via upload

---
+++ dzien2_zad5.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<math.h>
+int main(){
+int x; 
+for(x=0;x<=180;x=x+30){
+printf("sin(%3d)=%7.4lf  ", x,sin(x*M_PI/180));
+printf("cos(%3d)=%7.4lf\n", x,cos(x*M_PI/180));
+}
+return 0;
+}
